ad37c2cc48596b7f3a6482bf7a424a650552c36f
Alexey Karyshkov <alexeykaryshkov@Alexeys-MacBook-Pro.local>
2022-03-13 20:31:33 -0400

    add contact popup page

--- app/js/script.js
+++ app/js/script.js
@@ -119,3 +119,69 @@ document.addEventListener("click", makeActive);
 window.addEventListener("hashchange", setContentAccordingToHash);
 
 initializePage();
+
+// --------------
+
+var modalOuter = document.querySelector(".modal-outer");
+var modalInner = document.querySelector(".modal");
+
+var betaContent = `
+<h3>Oooops!</h3>
+<p>Wow! Nothing works!<p>
+`;
+var buttonContent = `
+<h2>Coming Soon</h2>
+<p>This feature coming soon.<p>
+
+          <!-- <form name="contact" method="POST" action="/" autocomplete="true"> -->
+          <form action="/" data-netlify="true">
+            
+            
+              <label for="name">Your name</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Name"
+                required
+                autocomplete="name"
+                title="Please enter your name"
+              />
+
+              <label for="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                autocomplete="email"
+                title="The domain portion of the email address is invalid (the portion after the @)."
+              />
+
+              <textarea
+                name="message"
+                id="message"
+                placeholder="Your message"
+                rows="7"
+              ></textarea>
+
+              <button type="submit" name="submit">Send Message</button>
+            
+          </form>
+
+<a class="closer" href="#0">✖︎</a>
+`;
+
+function showPopover(event) {
+  if (event.target.matches(".beta")) {
+    modalInner.innerHTML = betaContent;
+    modalOuter.classList.add("open");
+  } else if (event.target.closest("button")) {
+    modalInner.innerHTML = buttonContent;
+    modalOuter.classList.add("open");
+  } else if (event.target.matches(".closer, .modal-outer")) {
+    modalOuter.classList.remove("open");
+  } else return;
+  event.preventDefault();
+}
+
+document.addEventListener("click", showPopover);
